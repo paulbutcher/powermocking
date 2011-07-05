66370ba429d060cdc196da05b9e120a68f69ff77
Paul Butcher <paul@paulbutcher.com>
2011-07-05 15:31:50 +0100

    Switch to using FunSuite

--- src/test/scala/WarehouseTest.scala
+++ src/test/scala/WarehouseTest.scala
@@ -1,55 +1,47 @@
 package com.example.warehouse
 
-import org.scalatest.WordSpec
+import org.scalatest.FunSuite
 import com.borachio.generated.GeneratedMockFactory
 import com.borachio.scalatest.MockFactory
 import com.borachio.VerboseErrors
 import java.net.URL
 
-class WarehouseTest extends WordSpec with MockFactory with GeneratedMockFactory with VerboseErrors {
+class WarehouseTest extends FunSuite with MockFactory with GeneratedMockFactory with VerboseErrors {
   
   def mockClassPath = new URL("file:target/scala_2.9.0/mock-classes/")
   
-  "An order" when {
-    "in stock" should {
-      "remove inventory" in {
-        val mockWarehouse = mockObject(Warehouse)
-        inSequence {
-          mockWarehouse.expects.hasInventory("Talisker", 50) returning true once;
-          mockWarehouse.expects.remove("Talisker", 50) once
-        }
-        
-        val order = Order("Talisker", 50)
-        order.fill
-        
-        assert(order.filled)
-      }
+  test("Enough stock") {
+    val mockWarehouse = mockObject(Warehouse)
+    inSequence {
+      mockWarehouse.expects.hasInventory("Talisker", 50) returning true once;
+      mockWarehouse.expects.remove("Talisker", 50) once
     }
     
-    "not in stock" should {
-      "remove nothing" in {
-        val mockWarehouse = mockObject(Warehouse)
-        mockWarehouse.expects.hasInventory(*, *) returning false once
-        
-        val order = Order("Talisker", 50)
-        order.fill
-        
-        assert(!order.filled)
-      }
+    val order = Order("Talisker", 50)
+    order.fill
+    
+    assert(order.filled)
+  }
+  
+  test("Not enough stock") {
+    val mockWarehouse = mockObject(Warehouse)
+    mockWarehouse.expects.hasInventory(*, *) returning false once
+    
+    val order = Order("Talisker", 50)
+    order.fill
+    
+    assert(!order.filled)
+  }
+  
+  test("Order everything") {
+    val mockWarehouse = mockObject(Warehouse)
+    val mockOrder = mock[Order]
+    
+    inSequence {
+      mockWarehouse.expects.inStock("Laphroig") returning 10
+      mockOrder.expects.newInstance("Laphroig", 10)
     }
     
-    "as large as possible" should {
-      "consume all inventory" in {
-        val mockWarehouse = mockObject(Warehouse)
-        val mockOrder = mock[Order]
-        
-        inSequence {
-          mockWarehouse.expects.inStock("Laphroig") returning 10
-          mockOrder.expects.newInstance("Laphroig", 10)
-        }
-        
-        Order.largestPossible("Laphroig")
-      }
-    }
+    Order.largestPossible("Laphroig")
   }
 }
